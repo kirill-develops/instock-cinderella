698424029ab794546c94c485a6e72dae7e40509d
mahafawaad <maha.mushtaq15@gmail.com>
2022-03-16 17:53:57 -0400

    WIP - styling add warehouse page

--- src/components/NewWarehouseDetails/NewWarehouseDetails.js
+++ src/components/NewWarehouseDetails/NewWarehouseDetails.js
@@ -1,100 +1,111 @@
-import React from 'react'
-import './NewWarehouseDetails.scss'
+import React from 'react';
+import './NewWarehouseDetails.scss';
+// import { Link } from 'react-router-dom';
 
-function NewWarehouseDetails() {
+function NewWarehouseDetails({ clickHandler }) {
     return (
-        <>
-                <div className='new-warehouse__border'></div>
-                <h2>Warehouse Details</h2>
-            <form className='new-warehouse__form'>
-                <label
-                    className='new-warehouse__label'
-                    htmlFor="name">
-                    Warehouse Name
-                </label>
-                <input
-                    className="new-warehouse__input-field"
-                    placeholder="Warehouse Name"
-                    name="name"
-                />
-                <label
-                    className='new-warehouse__label'
-                    htmlFor="address">
-                    Street Address
-                </label>
-                <input
-                    className="new-warehouse__input-field"
-                    placeholder="Street Address"
-                    name="adress"
-                />
-                <label
-                    className='new-warehouse__label'
-                    htmlFor="city">
-                    City
-                </label>
-                <input
-                    className="new-warehouse__input-field"
-                    placeholder="City"
-                    name="city"
-                />
-                <label
-                    className='new-warehouse__label'
-                    htmlFor="country">
-                    Country
-                </label>
-                <input
-                    className="new-warehouse__input-field"
-                    placeholder="Country"
-                    name="country"
-                />
-                <div className='new-warehouse__border'></div>
-                <h2>Contact Details</h2>
-                <label
-                    className='new-warehouse__label'
-                    htmlFor="contact-name">
-                    Contact Name
-                </label>
-                <input
-                    className="new-warehouse__input-field"
-                    placeholder="Contact Name"
-                    name="contact-name"
-                />
-                <label
-                    className='new-warehouse__label'
-                    htmlFor="position">
-                    Position
-                </label>
-                <input
-                    className="new-warehouse__input-field"
-                    placeholder="Position"
-                    name="position"
-                />
-                <label
-                    className='new-warehouse__label'
-                    htmlFor="phone-number">
-                    Phone Number
-                </label>
-                <input
-                    className="new-warehouse__input-field"
-                    placeholder="Phone Number"
-                    name="phone-number"
-                />
-                <label
-                    className='new-warehouse__label'
-                    htmlFor="email">
-                    Email
-                </label>
-                <input
-                    className="new-warehouse__input-field"
-                    placeholder="Email"
-                    name="email"
-                />
+        <div className='new-warehouse'>
+            <div className='new-warehouse__border-top'></div>
+            <form
+                className='new-warehouse__form'
+                onSubmit={clickHandler}>
+                <div className='new-warehouse__form-fields'>
+                    <h2 className='new-warehouse__form-title'>Warehouse Details</h2>
+                    <label
+                        className='new-warehouse__label'
+                        htmlFor="warehouseName">
+                        Warehouse Name
+                    </label>
+                    <input
+                        className="new-warehouse__input-field"
+                        placeholder="Warehouse Name"
+                        name="warehouseName"
+                    />
+                    <label
+                        className='new-warehouse__label'
+                        htmlFor="address">
+                        Street Address
+                    </label>
+                    <input
+                        className="new-warehouse__input-field"
+                        placeholder="Street Address"
+                        name="address"
+                    />
+                    <label
+                        className='new-warehouse__label'
+                        htmlFor="city">
+                        City
+                    </label>
+                    <input
+                        className="new-warehouse__input-field"
+                        placeholder="City"
+                        name="city"
+                    />
+                    <label
+                        className='new-warehouse__label'
+                        htmlFor="country">
+                        Country
+                    </label>
+                    <input
+                        className="new-warehouse__input-field"
+                        placeholder="Country"
+                        name="country"
+                    />
+                </div>
+                <div className='new-warehouse__border-bottom'></div>
+                <div className='new-warehouse__form-fields'>
+                    <h2 className='new-warehouse__form-title'>Contact Details</h2>
+                    <label
+                        className='new-warehouse__label'
+                        htmlFor="contactName">
+                        Contact Name
+                    </label>
+                    <input
+                        className="new-warehouse__input-field"
+                        placeholder="Contact Name"
+                        name="contactName"
+                    />
+                    <label
+                        className='new-warehouse__label'
+                        htmlFor="position">
+                        Position
+                    </label>
+                    <input
+                        className="new-warehouse__input-field"
+                        placeholder="Position"
+                        name="position"
+                    />
+                    <label
+                        className='new-warehouse__label'
+                        htmlFor="phone">
+                        Phone Number
+                    </label>
+                    <input
+                        className="new-warehouse__input-field"
+                        placeholder="Phone Number"
+                        name="phone"
+                        type='number'
+                    />
+                    <label
+                        className='new-warehouse__label'
+                        htmlFor="email">
+                        Email
+                    </label>
+                    <input
+                        className="new-warehouse__input-field"
+                        placeholder="Email"
+                        name="email"
+                        type='email'
+                    />
+                </div>
                 <div className='new-warehouse__buttons'>
-                    <button className='new-warehouse__cancel-button'>Cancel</button>
-                    <button className='new-warehouse__add-button'>Add Warehouse</button>
+                    {/* <Link to='/warehouses'> */}
+                        <button className='new-warehouse__cancel-button'>Cancel</button>
+                    {/* </Link> */}
+                    <button className='new-warehouse__add-button' type='submit'>Add Warehouse</button>
                 </div>
             </form>
-        </>
+        </div>
     )
 }
 
--- src/pages/AddWarehousePage/AddWarehousePage.js
+++ src/pages/AddWarehousePage/AddWarehousePage.js
@@ -1,28 +1,65 @@
-import React from 'react';
+import React, { Component } from 'react';
 // import { Link } from 'react-router-dom';
 import backArrow from '../../assets/icons/arrow_back-24px.svg';
 import NewWarehouseDetails from '../../components/NewWarehouseDetails/NewWarehouseDetails'
 import axios from 'axios';
+import { BASE_URL } from '../../utils/api';
+import './AddWarehousePage.scss';
 
 
-const AddWarehousePage = () => {
+class AddWarehousePage extends Component {
+    // state = {
+    //     name: '',
+    //     address: '',
+    //     city: '',
+    //     country: '',
+    //     contactName: '',
+    //     position: '',
+    //     phone: '',
+    //     email: '',
+    //     errors: {},
+    // }
 
-    // add event handler and post request
+    submitHandler = (event) => {
+        event.preventDefault();
 
-  return (
-      <>
-          {/* <Header /> */}
-          <div className='add-warehouse__top'>
-    <img
-        className='add-warehouse__icon'
-        src={backArrow}
-        alt="back arrow icon"
-    />
-    <h1 className='add-warehouse__title'>Add New Warehouse</h1>
-              <NewWarehouseDetails />
-              </div>
-</>
-  )
+        axios
+            .post(`${BASE_URL}/warehouses`, {
+                name: event.target.warehouseName.value,
+                address: event.target.address.value,
+                city: event.target.city.value,
+                country: event.target.country.value,
+                contactName: event.target.contactName.value,
+                position: event.target.position.value,
+                phone: event.target.phone.value,
+                email: event.target.email.value
+            })
+            .then(response => {
+                console.log(response)
+                alert("New warehouse added!");
+                // history.push('/warehouses');
+            })
+            .catch(error => {
+                console.log(error);
+                alert("Unable to add warehouse");
+            });
+    }
+    render() {
+        return (
+            <div className='add-warehouse'>
+                {/* <Header /> */}
+                <div className='add-warehouse__top'>
+                    <img
+                        className='add-warehouse__icon'
+                        src={backArrow}
+                        alt="back arrow icon"
+                    />
+                    <h1 className='add-warehouse__title'>Add New Warehouse</h1>
+                    </div>
+                    <NewWarehouseDetails clickHandler={this.submitHandler} />
+            </div>
+        )
+    }
 }
 
 export default AddWarehousePage

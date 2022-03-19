7c293463142d2becd98a031884b0a9ca31979e56
Kirill Tchentsov <kirill.develops@gmail.com>
2022-03-19 15:40:32 -0400

    added edit & back buttons w/ functionality

--- src/App.js
+++ src/App.js
@@ -19,9 +19,9 @@ const App = () => {
         <Route path="/warehouses/:id/edit" component={EditWarehouse} />
         <Route path="/warehouses/:id" exact component={WarehouseDetails} />
         <Route path="/warehouses" exact render={(routerProps) => <WarehouseList {...routerProps} />} />
-        {/* <Route path="/inventory/:id/edit" render={<h1>"Edit Specific Inventory" Page WIP</h1>} /> */}
+        <Route path="/inventory/:id/edit" render={() => <h1>"Edit Specific Inventory" Page WIP</h1>} />
         <Route path="/inventory/:id" component={ItemDetails} />
-        {/* <Route path="/inventory/add" exact render={<h1>"Add New Inventory" Page WIP</h1>} /> */}
+        <Route path="/inventory/add" exact render={() => <h1>"Add New Inventory" Page WIP</h1>} />
         <Route path="/inventory" exact component={InventoryList} />
         <Redirect from="/" to="/warehouses" />
       </Switch>
--- src/components/ItemDetails/ItemDetails.js
+++ src/components/ItemDetails/ItemDetails.js
@@ -1,20 +1,26 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import apiUtils from '../../utils/apiUtils';
+
+import ArrowBack from '../../assets/icons/arrow_back-24px.svg';
+import EditIco from '../../assets/icons/edit-24px-white.svg';
 import './ItemDetails.scss';
 
-import EditIco from '../../assets/icons/edit-24px.svg';
-
+// Error message for bad API response
+const errorMessage = < p > Error fetching data, please try reloading in a few moments. If problem persists, URL may be incorrect.</p >
 class ItemDetails extends Component {
+
   state = {
     item: null
   }
 
-  componentDidMount() {
-    const errorMessage = < p > Error fetching data, please try reloading in a few moments</p >
 
+  componentDidMount() {
+
+    // API GET REQ for specific inventory ID based on URL
     apiUtils.getInventoryById(this.props.match.params.id)
       .then(res => {
-        console.log(res.data);
+        // After Validation, setState
         this.setState({ item: res.data });
       }).catch(err => {
         console.log(err);
@@ -23,7 +29,6 @@ class ItemDetails extends Component {
   };
 
   componentDidUpdate(prevProps) {
-    const errorMessage = < p > Error fetching data, please try reloading in a few moments</p >
 
     // deconstruct current and previous params
     const { id: currentId } = this.props.match.params
@@ -45,24 +50,32 @@ class ItemDetails extends Component {
   };
 
   handleDelete = (id) => {
-
   }
 
   render() {
 
+    // display during API GET req completed
     if (!this.state.item) {
       return <p>Loading...</p>;
     }
-    console.log(this.state.item);
-    const { description, category, itemName, warehouseName, quantity, status } = this.state.item;
+
+    const { description, category, itemName, warehouseName, quantity, status, id } = this.state.item;
 
     return (
 
       <div className='item-details'>
         <div className='item-details__inner'>
-          <h2 className='item-details__title'>
-            {itemName}
-          </h2>
+          <div className='item-details__headline'>
+            <div
+              className='item-details__button-back' onClick={() => this.props.history.goBack()}>
+              <img src={ArrowBack} alt="back button" />
+            </div>
+            <h2 className='item-details__title'>{itemName} </h2>
+            <Link to={`/inventory/${id}/edit`} className='item-details__button-edit'>
+              <img src={EditIco} alt="edit button" className='item-details__icon' />
+              <span className='item-details__button-txt'>Edit</span>
+            </Link>
+          </div>
           <div className='item-details__block'>
             <div className='item-details__highlights'>
               <label className='item-details__label--description'>

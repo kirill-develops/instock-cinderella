9cb191db47924eff2b2239a3b0ee2207e1fee284
Jacob Martella <jacobmartella@gmail.com>
2022-03-19 10:38:26 -0400

    Reformatted code via prettier.

--- src/components/EditInventoryItem/EditInventoryItem.js
+++ src/components/EditInventoryItem/EditInventoryItem.js
@@ -37,50 +37,50 @@ function EditInventoryItem() {
                     <option>1</option>
                   </select>
                 </div>
-            </div>
+              </div>
 
-                <div className="inventory__card inventory__card--divider">
-                  <h3 className="inventory__subheader">Item Availability</h3>
-                  <div className="inventory__availability">
-                    <label className="inventory__label">Status</label>
-                    <div className="inventory__details">
-                      <div className="inventory__stockpile">
-                        <input
-                          className="inventory__radio"
-                          type="radio"
-                          id="instock"
-                          name="instock"
-                          value="In Stock"
-                          for="instock"
-                        ></input>
-                        <label className="inventory__instock" for="instock">
-                          In Stock
-                        </label>
-                      </div>
-                      <div className="inventory__stockpile">
-                        <input
-                          className="inventory__radio"
-                          type="radio"
-                          id="outstock"
-                          name="outstock"
-                          value="Out of stock"
-                          for="outstock"
-                        ></input>
-                        <label className="inventory__outstock" for="outstock">
-                          Out of stock
-                        </label>
-                        <div></div>
-                      </div>
+              <div className="inventory__card inventory__card--divider">
+                <h3 className="inventory__subheader">Item Availability</h3>
+                <div className="inventory__availability">
+                  <label className="inventory__label">Status</label>
+                  <div className="inventory__details">
+                    <div className="inventory__stockpile">
+                      <input
+                        className="inventory__radio"
+                        type="radio"
+                        id="instock"
+                        name="instock"
+                        value="In Stock"
+                        for="instock"
+                      ></input>
+                      <label className="inventory__instock" for="instock">
+                        In Stock
+                      </label>
                     </div>
-                    <div className="inventory__block">
-                      <label className="inventory__label">Warehouse</label>
-                      <select className="inventory__dropdown">
-                        <option>List of Warehouses Goes here</option>
-                        <option>1</option>
-                      </select>
+                    <div className="inventory__stockpile">
+                      <input
+                        className="inventory__radio"
+                        type="radio"
+                        id="outstock"
+                        name="outstock"
+                        value="Out of stock"
+                        for="outstock"
+                      ></input>
+                      <label className="inventory__outstock" for="outstock">
+                        Out of stock
+                      </label>
+                      <div></div>
                     </div>
                   </div>
+                  <div className="inventory__block">
+                    <label className="inventory__label">Warehouse</label>
+                    <select className="inventory__dropdown">
+                      <option>List of Warehouses Goes here</option>
+                      <option>1</option>
+                    </select>
+                  </div>
                 </div>
+              </div>
             </form>
           </div>
         </div>
